1d2b78d2708bc0f6a9c02b19b27ae67d7ec7713c
creeds-knight <apedoarthur21@gmail.com>
2024-02-26 16:27:17 +0300

    initial commit on task 4 and 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - A function to check if the node is a root
+ * @node: Node to be checked
+ * Return: 1 if it is 0 otherwise
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if(node->parent == NULL)
+		return (1);
+	return (0);
+}
