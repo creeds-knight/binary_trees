7c0f5f81898d9b93e9617ef23ce83cf8f08eae30
creeds-knight <apedoarthur21@gmail.com>
2024-02-28 23:05:32 +0300

    task-4 betty correction

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -4,18 +4,6 @@
  * @node: Node to be checked
  * Return: 1 if is a leaf 0 otherwise
  */
-/**
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node == NULL)
-		return (0);
-	if (node->parent != NULL)
-	{
-		if (node->left == NULL && node->right == NULL)
-			return (1);
-		return (0);
-	}
-}*/
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL || node->left != NULL || node->right != NULL)
