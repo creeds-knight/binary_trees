2949fab270d8f95165c8d740bfe89357c5914ec8
creeds-knight <apedoarthur21@gmail.com>
2024-02-28 23:01:49 +0300

    task-4correction

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -4,7 +4,7 @@
  * @node: Node to be checked
  * Return: 1 if is a leaf 0 otherwise
  */
-
+/**
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
@@ -15,5 +15,11 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 			return (1);
 		return (0);
 	}
-	return (0);
+}*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
+
+	return (1);
 }
